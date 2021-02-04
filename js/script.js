d847751508d5e6af512f4345aae8d64e060489b5
sakity555 <mayl.e2580w@gmail.com>
2021-02-05 01:39:06 +0900

    jQuery追加

--- js/script.js
+++ js/script.js
@@ -4,6 +4,35 @@ $(function () {
     });
 });
 
+$(function () {
+    $(window).scroll(function () {
+        $(".main").each(function () {
+            var elemPos = $(this).offset().top; /* 要素の位置を取得 */
+            var scroll = $(window).scrollTop(); /* スクロール位置を取得 */
+            var windowHeight = $(window).height(); /* 画面幅を取得（画面の下側に入ったときに動作させるため） */
+            if (scroll > elemPos - windowHeight) {
+                /* 要素位置までスクロール出来たときに動作する */
+                $(this).addClass("main-scroll");
+            }
+        });
+    });
+    jQuery(window).scroll();
+});
+$(function () {
+    $(window).scroll(function () {
+        $(".sec01").each(function () {
+            var elemPos = $(this).offset().top; /* 要素の位置を取得 */
+            var scroll = $(window).scrollTop(); /* スクロール位置を取得 */
+            var windowHeight = $(window).height(); /* 画面幅を取得（画面の下側に入ったときに動作させるため） */
+            if (scroll > elemPos - windowHeight) {
+                /* 要素位置までスクロール出来たときに動作する */
+                $(this).addClass("sec01-scroll");
+            }
+        });
+    });
+    jQuery(window).scroll();
+});
+
 var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     /*centeredSlides: true,*/
